febd817286f8a7f78a36a39b332592d7404c84a6
gavan <gavan@leoedu.cn>
2016-04-18 10:45:53 +0800

    gu.

---
+++ gu/examples/src/example/xml/FlashMemory.java
@@ -0,0 +1,9 @@
+package example.xml;
+
+import java.util.Collections;
+
+public class FlashMemory implements Contacts {
+    public Iterable<Contact> findByName(String name) {
+        return Collections.emptyList();
+    }
+}
